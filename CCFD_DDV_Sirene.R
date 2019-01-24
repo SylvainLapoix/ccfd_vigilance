ec2124e5bb8f6a0705c73e72c23c520399b86276
Sy <sylvainlapoix@gmail.com>
2019-01-24 19:24:09 +0100

    1er calcul Sirene : ét > 5000 salariés

--- CCFD_DDV_Sirene.R
+++ CCFD_DDV_Sirene.R
@@ -1,8 +1,9 @@
+## libs ----
+
 library(tidyverse)
 
-eff <- read_csv("data/trancheEffectifsEtablissement.csv")
-s <- read_csv("data/StockEtablissement_utf8.csv")
-headers <- read_csv("data/headers.csv")
+# s <- read_csv("data/StockEtablissement_utf8.csv")
+eff <- read_csv("data/effectifs.csv")
 
 names(s)
 
@@ -31,6 +32,28 @@ write_csv(s2, "./data/sirene_light.csv")
 write_csv(e_eff, "./data/slight_eff.csv")
 write_csv(e_noms, "./data/slight_noms.csv")
 
-## groupement ----
+## groupements par siren ----
 e_eff <- read_csv("./data/slight_eff.csv")
 e_noms <- read_csv("./data/slight_noms.csv")
+
+# première sélection simple : supérieur à 5000 par établissement
+e_5000FR <- e_eff %>% filter(trancheeffectifsetablissement %in% c("52","53"),
+                             is.na(libellepaysetrangeretablissement)) #tally() = 50
+
+table(e_5000$trancheeffectifsetablissement) # 52 = 45, 53 = 5
+
+names(e_noms)
+
+e_5000FR <- left_join(e_5000, e_noms, by = c("siren"="siren", "nic"="nic"))
+nomsE5000 <- e_5000FR %>% filter(!is.na(enseigne1etablissement)) %>% select(enseigne1etablissement) # tally() = 23
+e_5000FR %>% filter(!is.na(denominationusuelleetablissement)) # tally() = 1
+
+nomsE5000 %>% tbl_df() %>% print(n = Inf)
+
+# d'où vient le problème de non renseignement ? Problème dans le left_join() ?
+
+# deuxème sélection par groupement
+e_eff %>% filter(trancheeffectifsetablissement %in% c("01","02","03","11","12","21","22","31","32","41","42","51")) %>% 
+  group_by(siren,trancheeffectifsetablissement,libellepaysetrangeretablissement) %>% 
+  summarise(total = n())
+
--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 Repo du projet CCFD - Devoir de vigilance
 
 En cours.
+
+## Sources
+
+Primaires :
+* Sirene, stock établissements au 01/2019 : https://www.data.gouv.fr/fr/datasets/r/d9c12e7e-6a9d-4564-a846-86b8b563a477 (as "./data/StockEtablissement_utf8.csv") ;
+
+Secondaires :
+* BDD des tranches d'effectifs sur la base de la doc (as "./data/trancheEffectifsEtablissement.csv")
+
+## Etat des recherches
+
+### Nettoyage
+Constitution de deux BDDs légères : une BDD effectifs et une BDD noms sur la base d'une version filtrée (exclusion des établissements fermés et des effectifs nuls ou sans effectifs).
+
+### Calculs
+
+*Première approche : quanti strict par siren*
+Premier groupe des entreprises avec des effectifs en France > 5000 :
+* 50 matchs ;
+* seulement 23 avec "enseigne1etablissement" renseigné ;
+* seulement 1 avec "denominationusuelleetablissement" renseigné (déjà incluse dans les 23 précédentes).
+
+Deux sociétés privées apparaissent dans ce groupement :
+* **DISNEYLAND PARIS-DISNEY VILLAGE-WALT DISNEY STUDIO** ;
+* **ALTEN JEAN JAURES**.
+
